26d30e3f0341e4fef3440678e351e937481eea70
Christoph551 <christophersimmonds551@gmail.com>
2023-01-24 16:35:11 -0700

    Initial Commit

---
+++ Tests/Employee.test.js
@@ -0,0 +1,66 @@
+// Instructor Rachel Thiim recommended using these exact tests to complete Module 10's Challenge
+
+const Employee = require("../lib/Employee");
+
+test("When used with the \"new\" keyword, instantiates an Employee object", () => {
+  // Arrange
+  const e = new Employee();
+  // Assert
+  expect(typeof(e)).toBe("object");
+});
+
+test("The first constructor argument will set the \"name\" property", () => {
+  // Arrange
+  const name = "Mavis";
+  const e = new Employee(name);
+  // Assert
+  expect(e.name).toBe(name);
+});
+
+test("The second constructor argument will set the \"id\" property", () => {
+  // Arrange
+  const controlVal = 1;
+  const e = new Employee("Mavis", controlVal);
+  // Assert
+  expect(e.id).toBe(controlVal);
+});
+
+test("The third constructor argument will set the \"email\" property", () => {
+  // Arrange
+  const controlVal = "test@email.com";
+  const e = new Employee("Mavis", 1, controlVal);
+  // Assert
+  expect(e.email).toBe(controlVal);
+});
+
+test("getName() returns a \"name\" string", () => {
+  // Arrange
+  const controlVal = "Mavis";
+  const e = new Employee(controlVal);
+  // Assert
+  expect(e.getName()).toBe(controlVal);
+});
+
+test("getId() returns an \"id\" integer", () => {
+  // Arrange
+  const controlVal = 1;
+  const e = new Employee("Mavis", controlVal);
+  // Assert
+  expect(e.getId()).toBe(controlVal);
+});
+
+test("getEmail() returns an \"email\" string", () => {
+  // Arrange
+  const controlVal = "test@email.com";
+  const e = new Employee("Mavis", 1, controlVal);
+  // Assert
+  expect(e.getEmail()).toBe(controlVal);
+});
+
+test("getRole() returns an \"Employee\" string", () => {
+  // Arrange
+  const controlVal = "Employee";
+  const e = new Employee("Mavis", 1, "test@email.com");
+  // Assert
+  expect(e.getRole()).toBe(controlVal);
+});
---
+++ Tests/Manager.test.js
@@ -0,0 +1,27 @@
+// Instructor Rachel Thiim recommended using these exact tests to complete Module 10's Challenge
+
+const Manager = require("../lib/Manager");
+
+test("The fourth constructor argument will set the \"officeNumber\" property", () => {
+  // Arrange
+  const controlVal = 202;
+  const e = new Manager("Mavis", 1, "test@email.com", controlVal);
+  // Assert
+  expect(e.officeNumber).toBe(controlVal);
+});
+
+test("getRole() returns a \"Manager\" string", () => {
+  // Arrange
+  const controlVal = "Manager";
+  const e = new Manager("Mavis", 1, "test@email.com", 202);
+  // Assert
+  expect(e.getRole()).toBe(controlVal);
+});
+
+test("getOffice() returns an \"officeNumber\" string", () => {
+  // Arrange
+  const controlVal = 202;
+  const e = new Manager("Mavis", 1, "test@email.com", controlVal);
+  // Assert
+  expect(e.getOfficeNumber()).toBe(controlVal);
+});
